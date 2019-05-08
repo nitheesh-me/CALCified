393a0847e1c443598d8172e5a3791fad1bda5efb
nitheesh-me <nitheesh.me@yahoo.com>
2019-05-08 18:53:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -197,4 +197,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True,ssl_context='adhoc')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port,debug=False,ssl_context='adhoc')
